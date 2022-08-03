df6f4ad27de41066ac2f5581436140c3a4ba6018
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-03 16:22:37 +0500

    setup the context.js with contextAPI and states and custom hook

--- src/context.js
+++ src/context.js
@@ -1,2 +1,40 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+export const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+  return (
+    <AppProvider.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppProvider.Provider>
+  );
+};
+// Custom Hook
+export const useGlobalContext = () => {
+  useContext(AppContext);
+};
